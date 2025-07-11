16fd36b9ae9b40476a9fefe1872758bf4816480c
DevChetanMalviya <chetanmalviyaaws@gmail.com>
2025-07-11 17:25:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/about', (req,res) => {
     res.sendFile(path.join(__dirname+'/about.html'));
    
   });
-
+#app.listen(8081, () => {
 app.listen(8081, () => {
     console.log('Listening on port 8081');
 });
